194bdbd5800392f5bc4222f5be417c510aa80fec
elylathifah <12046124009@universitas-bth.ac.id>
2026-05-18 13:48:37 +0700

    Add files via upload

---
+++ SIGAP/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="container">
+    <p><strong>Sistem Pelaporan Kekerasan Seksual</strong></p>
+    <p>satgasppks@kampus.ac.id | 08987321309</p>
+  </div>
+</footer>
